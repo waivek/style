ea72a776f76a707aa95aeae7b138721ceffaee22
Vivek Bose <waivek@gmail.com>
2019-06-28 14:46:23 +0530

    Subsetted fonts

---
+++ fonts/subset/subset.bat
@@ -0,0 +1,6 @@
+@echo off
+rem pyftsubset confluence_c3_bold.woff --text-file=confluence_c3_bold_subset.txt --flavor=woff
+rem pyftsubset confluence_c3_regular.woff --text-file=confluence_c3_regular_subset.txt --flavor=woff
+rem pyftsubset fairplay_text_a_bold.woff --text-file=fairplay_text_a_bold_subset.txt --flavor=woff
+rem pyftsubset fairplay_text_a_italic.woff --text-file=fairplay_text_a_italic_subset.txt --flavor=woff
+pyftsubset fairplay_text_a_regular.woff --text-file=fairplay_text_a_regular_subset.txt --flavor=woff
